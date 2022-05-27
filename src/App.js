861be59c9001ea3dc021b43c6979bce731732ad3
Başak Dönder <basakdonder01@gmail.com>
2022-05-27 23:07:13 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,79 +1,107 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { QuizData } from "./QuizData";
 
 function App() {
   const [current, setCurrent] = useState(0);
-  const currentQuiz = QuizData[current];
+  const [point, setPoint] = useState(0);
   const [selectedOption, setSelectedOption] = useState("");
+  const [selectedRadio, setSelectedRadio] = useState();
+  const [isCompleted, setIsCompleted] = useState(false);
+  const currentQuiz = QuizData[current];
+
+  useEffect(() => {
+    console.log(point);
+  }, [point]);
 
   function nextQuestion() {
     if (QuizData.length > current) {
+      if (currentQuiz.answer === selectedOption) {
+        setPoint(point + 1);
+      }
+      if (current === QuizData.length - 1) {
+        setIsCompleted(true);
+      }
       setCurrent(current + 1);
-    } else {
-      alert("You finished the quiz!!!");
+      selectedRadio.checked = false;
+      setSelectedOption("");
     }
-    //console.log(selectedOption);
+  }
+
+  function handleSelectedOption(e) {
+    setSelectedOption(e.target.value);
+    setSelectedRadio(e.target);
   }
 
   return (
     <div className="quiz-container">
       <div className="container">
-        <h2>{currentQuiz.question}</h2>
-        <ul>
-          <li>
-            <input
-              type="radio"
-              name="answer"
-              id="a"
-              value="a"
-              onChange={(e) => setSelectedOption(e.target.value)}
-            />
-            <label htmlFor="a" id="a_text">
-              {currentQuiz.a}
-            </label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              name="answer"
-              id="b"
-              value="b"
-              onChange={(e) => setSelectedOption(e.target.value)}
-            />
-            <label htmlFor="b" id="b_text">
-              {currentQuiz.b}
-            </label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              name="answer"
-              id="c"
-              value="c"
-              onChange={(e) => setSelectedOption(e.target.value)}
-            />
-            <label htmlFor="c" id="c_text">
-              {currentQuiz.c}
-            </label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              name="answer"
-              id="d"
-              value="d"
-              onChange={(e) => setSelectedOption(e.target.value)}
-            />
-            <label htmlFor="d" id="d_text">
-              {currentQuiz.d}
-            </label>
-          </li>
-        </ul>
+        {current === QuizData.length ? (
+          <h2>
+            You did {point} of {QuizData.length}
+          </h2>
+        ) : (
+          <>
+            <h2>{currentQuiz.question}</h2>
+            <ul>
+              <li>
+                <input
+                  type="radio"
+                  name="answer"
+                  id="a"
+                  value="a"
+                  onChange={handleSelectedOption}
+                />
+                <label htmlFor="a" id="a_text">
+                  {currentQuiz.a}
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="answer"
+                  id="b"
+                  value="b"
+                  onChange={handleSelectedOption}
+                />
+                <label htmlFor="b" id="b_text">
+                  {currentQuiz.b}
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="answer"
+                  id="c"
+                  value="c"
+                  onChange={handleSelectedOption}
+                />
+                <label htmlFor="c" id="c_text">
+                  {currentQuiz.c}
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="answer"
+                  id="d"
+                  value="d"
+                  onChange={handleSelectedOption}
+                />
+                <label htmlFor="d" id="d_text">
+                  {currentQuiz.d}
+                </label>
+              </li>
+            </ul>
+          </>
+        )}
       </div>
 
-      <button className="submit-btn" onClick={() => nextQuestion()}>
-        Submit
+      <button
+        className={isCompleted ? "submit-btn hide" : "submit-btn"}
+        onClick={() => nextQuestion()}
+      >
+        {current === QuizData.length - 1 ? "Complete Quiz" : "Submit"}
       </button>
     </div>
   );
