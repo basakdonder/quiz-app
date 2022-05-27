cc61276cf3ce4820053050dc7bd2e97193e3b123
Başak Dönder <basakdonder01@gmail.com>
2022-05-27 03:11:53 +0300

    getting selected option
    creating quiz container and getting selected option

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { QuizData } from "./QuizData";
 
 function App() {
+  const [current, setCurrent] = useState(0);
+  const currentQuiz = QuizData[current];
+  const [selectedOption, setSelectedOption] = useState("");
+
+  function nextQuestion() {
+    if (QuizData.length > current) {
+      setCurrent(current + 1);
+    } else {
+      alert("You finished the quiz!!!");
+    }
+    //console.log(selectedOption);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quiz-container">
+      <div className="container">
+        <h2>{currentQuiz.question}</h2>
+        <ul>
+          <li>
+            <input
+              type="radio"
+              name="answer"
+              id="a"
+              value="a"
+              onChange={(e) => setSelectedOption(e.target.value)}
+            />
+            <label htmlFor="a" id="a_text">
+              {currentQuiz.a}
+            </label>
+          </li>
+          <li>
+            <input
+              type="radio"
+              name="answer"
+              id="b"
+              value="b"
+              onChange={(e) => setSelectedOption(e.target.value)}
+            />
+            <label htmlFor="b" id="b_text">
+              {currentQuiz.b}
+            </label>
+          </li>
+          <li>
+            <input
+              type="radio"
+              name="answer"
+              id="c"
+              value="c"
+              onChange={(e) => setSelectedOption(e.target.value)}
+            />
+            <label htmlFor="c" id="c_text">
+              {currentQuiz.c}
+            </label>
+          </li>
+          <li>
+            <input
+              type="radio"
+              name="answer"
+              id="d"
+              value="d"
+              onChange={(e) => setSelectedOption(e.target.value)}
+            />
+            <label htmlFor="d" id="d_text">
+              {currentQuiz.d}
+            </label>
+          </li>
+        </ul>
+      </div>
+
+      <button className="submit-btn" onClick={() => nextQuestion()}>
+        Submit
+      </button>
     </div>
   );
 }
---
+++ src/QuizData.js
@@ -0,0 +1,26 @@
+export const QuizData = [
+  {
+    question: "What is the color of blood when it's inside your body?",
+    a: "Blue",
+    b: "White",
+    c: "Red",
+    d: "Green",
+    answer: "c",
+  },
+  {
+    question: "Which color is not there in the rainbow?",
+    a: "Indigo",
+    b: "Brown",
+    c: "Red",
+    d: "Yellow",
+    answer: "b",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    a: "Caravaggio",
+    b: "Sandro Botticelli",
+    c: "Leonardo Da Vinci",
+    d: "Galileo Galilei",
+    answer: "c",
+  },
+];
